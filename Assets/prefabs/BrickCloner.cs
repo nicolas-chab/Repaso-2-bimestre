177fa79f21334ac2813be3cfc15c15e0a06ef8d4
nicolas-chab <101563616+nicolas-chab@users.noreply.github.com>
2022-05-17 15:47:08 -0300

    Initial commit

---
+++ Assets/prefabs/BrickCloner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BrickCloner : MonoBehaviour
+{
+    public GameObject brickPrefab;
+    public float xOffset;
+    int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(brickPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void CloneBrick()
+    {
+        if (counter < 5)
+        {
+
+
+            GameObject clon;
+            clon = Instantiate(brickPrefab);
+            //para destruir el cubo pongo Destroy(clon,2);
+            clon.transform.position = new Vector3(xOffset, 0, 0);
+            xOffset += 1.1f;
+            counter++;
+        }
+    }
+}
